51740eb287a89ecc1f2d1147960ada0b7e740f00
Mohamed Aboalmkarem <146967816+Aboalmkarem@users.noreply.github.com>
2024-12-24 16:23:02 +0200

    update message popup

--- main.js
+++ main.js
@@ -1,22 +1,31 @@
-const form = document.querySelector('#form');
-const MessageContent = document.querySelector('#popupMessage');
-const message = document.querySelector('#popup');
-const popupContainer = document.querySelector('.popup-container');
-const closePopup = document.querySelector('#closePopup')
+const form = document.querySelector("#form");
+const messageContent = document.querySelector(".messageContent");
+const message = document.querySelector(".message");
+const messageContainer = document.querySelector(".messageContainer");
+const closeBtn = document.querySelector(".closeBtn");
+const progressFill = document.querySelector(".progress-fill");
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
-    let content = event.target[0].value
-    let backgroundColor = event.target[1].value === 'success' ? '#45a049' : "#a43535"
-    showPopupMessage(content, backgroundColor)
-})
-
+    let content = event.target[0].value;
+    let backgroundColor =
+        event.target[1].value === "success" ? "#45a049" : "#a43535";
+    showPopupMessage(content, backgroundColor);
+});
 function showPopupMessage(content, backgroundColor) {
-    MessageContent.innerHTML = content
-    popupContainer.style.backgroundColor = backgroundColor
-    message.style.display = 'flex'
+    messageContent.innerHTML = content;
+    messageContainer.style.backgroundColor = backgroundColor;
+    message.style.animation = "swap 5s ease-in-out";
+    progressFill.style.animation = "increment 4.8s linear";
+    setTimeout(() => {
+        removePopupMessage()
+    }, 5000);
 }
-
-closePopup.addEventListener('click', () => {
-    message.style.display = 'none'
-})
+function removePopupMessage() {
+    message.style.animation = "";
+    progressFill.style.animation = "";
+    messageContainer.style.backgroundColor = "";
+}
+closeBtn.addEventListener("click", () => {
+    removePopupMessage()
+});
