ec15f608746203a0917a684c4c954b0a6c046af1
Mohamed Aboalmkarem <146967816+Aboalmkarem@users.noreply.github.com>
2024-12-24 20:53:03 +0200

    new update in message logic

--- main.js
+++ main.js
@@ -1,31 +1,50 @@
 const form = document.querySelector("#form");
-const messageContent = document.querySelector(".messageContent");
-const message = document.querySelector(".message");
-const messageContainer = document.querySelector(".messageContainer");
-const closeBtn = document.querySelector(".closeBtn");
-const progressFill = document.querySelector(".progress-fill");
+const popup = document.querySelector(".popup");
 
+let messages = [];
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     let content = event.target[0].value;
     let backgroundColor =
         event.target[1].value === "success" ? "#45a049" : "#a43535";
-    showPopupMessage(content, backgroundColor);
+    if (content !== '') {
+        addMessage(content, backgroundColor);
+    } else {
+        addMessage('message cannot be empty', '#a43535');
+    }
 });
-function showPopupMessage(content, backgroundColor) {
-    messageContent.innerHTML = content;
-    messageContainer.style.backgroundColor = backgroundColor;
-    message.style.animation = "swap 5s ease-in-out";
-    progressFill.style.animation = "increment 4.8s linear";
+
+function showNewMessage(messages) {
+    popup.innerHTML = messages.map(
+        (message) => `
+            <div>
+                <div class="message">
+                <div class="messageContainer" style='background-color: ${message.backgroundColor}'>
+                    <p class="messageContent">${message.content}</p>
+                    <span class="closeBtn" onclick='removeMessage(${message.id})'>&times;</span>
+                    <div class="progress">
+                        <div class="progress-fill"></div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        `
+    ).join('')   
+}
+
+function addMessage(content, backgroundColor) {
+    const newMessage = {
+        id: Date.now(),
+        content: content,
+        backgroundColor: backgroundColor,
+    };
+    messages = [...messages, newMessage];
+    showNewMessage(messages);
     setTimeout(() => {
-        removePopupMessage()
-    }, 5000);
+        removeMessage(newMessage.id);
+    }, 5001);
 }
-function removePopupMessage() {
-    message.style.animation = "";
-    progressFill.style.animation = "";
-    messageContainer.style.backgroundColor = "";
+function removeMessage(id) {
+    messages = messages.filter((message) => message.id !== id);
+    showNewMessage(messages)
 }
-closeBtn.addEventListener("click", () => {
-    removePopupMessage()
-});
